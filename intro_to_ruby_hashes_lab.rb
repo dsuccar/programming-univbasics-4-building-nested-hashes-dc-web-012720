1bacd4e186c7dae0ac6a4019c678b058627c1248
Daniel Succar <danielsuccar@icloud.com>
2020-01-21 22:33:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,5 +1,8 @@
 def base_hash
-	base_hash = {:new_hash => "1key"}
+	base_hash = {:new_hash =>
+		"1key",
+    "2key?"
+	}
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
 	{}
